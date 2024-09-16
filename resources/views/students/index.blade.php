97893766736387656295e1273eca3fd09ed8ded3
GowthamSelvaraj123 <gowthamdesigner94@gmail.com>
2024-09-16 13:37:29 +0530

    additional Enhancement

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -74,7 +74,7 @@
                                             <label class="block text-gray-700 text-sm font-bold mb-2">Marks</label>
                                             <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="students_marks" value="{{ old('students_marks', $student->students_marks) }}">
                                         </div>
-                                        <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Add</button>
+                                        <button class="primary-button inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Add</button>
                                     </form>
                                 </div>
                             </div>
@@ -107,13 +107,11 @@
                                 <label class="block text-gray-700 text-sm font-bold mb-2">Marks</label>
                                 <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="students_marks">
                             </div>
-                            <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Add</button>
+                            <button class="primary-button inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Add</button>
                         </form>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
-
 </x-app-layout>
