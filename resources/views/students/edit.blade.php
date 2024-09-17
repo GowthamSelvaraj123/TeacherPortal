366cd62a436d88de57b070d10ec5b46d62897610
GowthamSelvaraj123 <gowthamdesigner94@gmail.com>
2024-09-17 23:11:35 +0530

    input field & button text update

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -16,7 +16,7 @@
                         </div>
                         <div class="mb-4">
                         <label class="block text-gray-700 text-sm font-bold mb-2">Marks</label>
-                        <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="students_marks" value="{{ old('students_marks', $student->students_marks) }}">
+                        <input type="number" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="students_marks" value="{{ old('students_marks', $student->students_marks) }}">
                         </div>
                         <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Add</button>
                     </form>
